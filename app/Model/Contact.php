940c1e2bda5166971df496f3d523e8c561af7185
jinchangz <jinchangz1@gmail.com>
2021-03-11 10:09:46 +0900

    fix hitTail on table contents

--- app/Model/Contact.php
+++ app/Model/Contact.php
@@ -7,21 +7,21 @@ App::uses('AppModel', 'Model');
  */
 class Contact extends AppModel
 {
-    // テーブル使わない
-    public $useTable = false;
-    // バリデーションルール
-    public $validate = array(
-	    'email' => array(
-	    	array('rule' => 'email', 'required' => true,'message' => 'メールアドレスを正しく入力して下さい。')
-	    ),
-	    'body' => array(
-	    	array('rule' => 'notEmpty','required' => true, 'message' => '本文が入力されていません。')
-    	),
-	    'url' => array(
-	    	array('rule' => 'url', 'allowEmpty' => true,'message' => 'サイトURLを正しく入力して下さい。')
-    	),
-    	'rss' => array(
-	    	array('rule' => 'url', 'allowEmpty' => true,'message' => 'サイトRSSを正しく入力して下さい。')
-    	)
-    );
+	// テーブル使わない
+	public $useTable = false;
+	// バリデーションルール
+	public $validate = array(
+		'email' => array(
+			array('rule' => 'email', 'required' => true, 'message' => 'メールアドレスを正しく入力して下さい。')
+		),
+		'body' => array(
+			array('rule' => 'notBlank', 'required' => true, 'message' => '本文が入力されていません。')
+		),
+		'url' => array(
+			array('rule' => 'url', 'allowEmpty' => true, 'message' => 'サイトURLを正しく入力して下さい。')
+		),
+		'rss' => array(
+			array('rule' => 'url', 'allowEmpty' => true, 'message' => 'サイトRSSを正しく入力して下さい。')
+		)
+	);
 }
---
+++ app/View/Elements/alert.ctp
@@ -0,0 +1,5 @@
+<div class="alert alert-error">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    サーバーの移転を行いました。今後httpsへの変更を予定しております。<br>
+    その時サイトの設定がリセットされることになります。ご迷惑お掛けします。
+</div>
